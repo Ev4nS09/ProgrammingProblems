20bccadaab58da4ffa69666b8bc500238ef6f9ca
Ev4nS09 <afonsorio@gmail.com>
2024-10-01 23:59:33 +0100

    freya the frog start

--- codeforces/freya_the_frog.cpp
+++ codeforces/freya_the_frog.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+void solve()
+{
+    int x, y, d;
+    std::cin >> x >> y >> d;
+
+    int result = (ceil(((double)std::max((double) x, (double) y)) / d)) * 2;
+    
+    result = x - y + d > 0 ? result - 1 : result; 
+
+    std::cout << result << '\n';
+}
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
 
-    
+    int tests;
+    std::cin >> tests;
 
+    while(tests-- > 0)
+    {
+        solve();
+    }
+
+    return 0;
 }
